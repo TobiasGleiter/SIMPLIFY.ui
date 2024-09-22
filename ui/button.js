c2f85ec698410202ddc6dab9bff9a6d0afc02fb9
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-09-22 08:23:49 +0200

    feat: button light

--- ui/button.js
+++ ui/button.js
@@ -98,7 +98,33 @@ class ButtonDark extends HTMLElement {
   }
 }
 
+class ButtonLight extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
+      <link rel="stylesheet" href="ui/button.css" />
+      
+      <button
+        tabindex="0"
+        aria-pressed="false"
+        class="button button--light"
+      >
+        <slot>Button</slot>
+      </button>
+    `;
+  }
+}
+
 customElements.define('button--base', ButtonBase);
 customElements.define('button--primary', ButtonPrimary);
 customElements.define('button--secondary', ButtonSecondary);
+customElements.define('button--light', ButtonLight);
 customElements.define('button--dark', ButtonDark);
