61849c28778cfe595ef9b657b078da69cb033197
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-09-23 14:40:54 +0200

    chore: tried to implement new forms

--- js/base.js
+++ js/base.js
@@ -3,10 +3,17 @@ const messageForm = document.getElementById('messageForm');
 messageForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const userName = user_name.shadowRoot.querySelector('input').value;
-  const userMail = user_mail.shadowRoot.querySelector('input').value;
-  const userMessage = user_message.shadowRoot.querySelector('textarea').value;
-  const userSex = user_sex.shadowRoot.querySelector('select').value;
+  const isUserNameValid = user_name.validate();
+  const isUserMailValid = user_mail.validate();
 
-  console.log('Form data:', userName, userMail, userSex, userMessage);
+  console.log(isUserNameValid, isUserMailValid);
+
+  if (isUserNameValid && isUserMailValid) {
+    const userName = user_name.shadowRoot.querySelector('input').value;
+    const userMail = user_mail.shadowRoot.querySelector('input').value;
+    const userMessage = user_message.shadowRoot.querySelector('textarea').value;
+    const userSex = user_sex.shadowRoot.querySelector('select').value;
+
+    console.log('Form data:', userName, userMail, userSex, userMessage);
+  }
 });
--- ui/input.js
+++ ui/input.js
@@ -1,3 +1,5 @@
+//  REFACTOR WITH: https://www.dannymoerkerke.com/blog/native-form-validation-of-web-components/
+
 class InputBase extends HTMLElement {
   static formAssociated = true;
 
@@ -8,18 +10,27 @@ class InputBase extends HTMLElement {
   }
 
   render() {
-    const inputId = this.getAttribute('id') || '';
-    const placeholder = this.getAttribute('placeholder') || '';
+    const inputId = this.getAttribute('id') ? `id="${this.getAttribute('id')}"` : '';
     const type = this.getAttribute('type') || 'text';
+    const placeholder = this.getAttribute('placeholder') ? `placeholder="${this.getAttribute('placeholder')}"` : '';
+
+    const required = this.hasAttribute('required') ? 'required' : '';
+    const minLength = this.getAttribute('minlength') ? `minlength="${this.getAttribute('minlength')}"` : '';
+    const maxLength = this.getAttribute('maxlength') ? `maxlength="${this.getAttribute('maxlength')}"` : '';
+    const pattern = this.getAttribute('pattern') ? `pattern="${this.getAttribute('pattern')}"` : '';
 
     this.shadowRoot.innerHTML = `
       <link rel="stylesheet" href="ui/input.css" />
-      
       <label class="input">
         <slot></slot>
-        <input class="input__text" id="${inputId}" type="${type}" placeholder="${placeholder}" aria-placeholder="${placeholder}" />
-      </label
-      `;
+        <input class="input__text" id="${inputId}" type="${type}" ${placeholder} ${required} ${minLength} ${maxLength} ${pattern} />
+        <span class="error-message">Invalid input</span>
+      </label>`;
+  }
+
+  validate() {
+    const input = this.shadowRoot.querySelector('input');
+    return input.checkValidity();
   }
 }
 
