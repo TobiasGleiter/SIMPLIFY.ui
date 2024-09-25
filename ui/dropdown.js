f4fe22f6fea058b33934184173c9c92c79b0302c
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-09-25 13:17:08 +0200

    refactor: dropdown items

--- ui/dropdown.js
+++ ui/dropdown.js
@@ -29,7 +29,60 @@ class Dropdown extends HTMLElement {
     button.addEventListener('click', () => {
       dropdown.classList.toggle('active');
     });
+
+    window.addEventListener('click', (e) => {
+      if (!dropdown.contains(e.target)) {
+        dropdown.classList.remove('active');
+      }
+    });
+
+    dropdown.addEventListener('click', (e) => {
+      e.stopPropagation();
+    });
+  }
+}
+
+class DropdownLink extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.render();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
+      <link rel="stylesheet" href="ui/dropdown.css" />
+      <a class="dropdown__link"><slot></slot></a>
+    `;
+  }
+
+  connectedCallback() {
+    this.input = this.shadowRoot.querySelector('a');
+    ['href'].forEach((attr) => {
+      const attrValue = attr === 'required' ? this.hasAttribute(attr) : this.getAttribute(attr);
+
+      if (attrValue !== null && attrValue !== undefined) {
+        this.input[attr] = attrValue;
+      }
+    });
+  }
+}
+
+class DropdownItem extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.render();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
+      <link rel="stylesheet" href="ui/dropdown.css" />
+      <span class="dropdown__item"><slot></slot></span>
+    `;
   }
 }
 
 customElements.define('dropdown--base', Dropdown);
+customElements.define('dropdown__link--base', DropdownLink);
+customElements.define('dropdown__item--base', DropdownItem);
