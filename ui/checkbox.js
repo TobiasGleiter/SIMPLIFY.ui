4857c92b22c92fdb3b0c904451c65b6818b4bb4f
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-09-26 18:46:10 +0200

    chore: checkbox disabled and checked

--- ui/checkbox.js
+++ ui/checkbox.js
@@ -2,24 +2,42 @@ class CheckboxBase extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
-  }
-
-  connectedCallback() {
     this.render();
   }
 
-  render() {
-    const checked = this.getAttribute('checked');
+  connectedCallback() {
+    this.addAttributes();
+  }
 
+  render() {
     this.shadowRoot.innerHTML = `
     <link rel="stylesheet" href="ui/checkbox.css" />
 
     <label class="checkbox">
-        <input type="checkbox" ${checked}>
-        <span class="checkmark"></span>
-        <slot></slot>
+        <input type="checkbox">
+        <span class="checkbox__checkmark"></span>
+        <slot class="checkbox__text"></slot>
     </label>
     `;
+  }
+
+  addAttributes() {
+    this.input = this.shadowRoot.querySelector('input[type="checkbox"]');
+    ['name', 'value', 'checked', 'disabled'].forEach((attr) => {
+      const attrValue = attr === 'required' ? this.hasAttribute(attr) : this.getAttribute(attr);
+
+      if (attrValue !== null && attrValue !== undefined) {
+        this.input[attr] = attrValue;
+      }
+    });
+
+    if (this.hasAttribute('checked')) {
+      this.input.checked = true;
+    }
+
+    if (this.hasAttribute('disabled')) {
+      this.input.disabled = true;
+    }
   }
 }
 
