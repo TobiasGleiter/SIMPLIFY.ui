29783c5e7d25d7df9956f3baf7fe825125b5d142
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-10-01 07:33:03 +0200

    feat: simple breadcrumb

---
+++ ui/breadcrumb.js
@@ -0,0 +1,74 @@
+class BreadcrumbBase extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.render();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
+        <link rel="stylesheet" href="ui/breadcrumb.css" />
+        <div class="breadcrumb"></div>
+      `;
+
+    this.processBreadcrumbs();
+  }
+
+  processBreadcrumbs() {
+    const slot = document.createElement('slot');
+    const breadcrumbContainer = this.shadowRoot.querySelector('.breadcrumb');
+    breadcrumbContainer.appendChild(slot);
+
+    const assignedNodes = slot.assignedElements();
+
+    if (assignedNodes.length > 4) {
+      breadcrumbContainer.innerHTML = '';
+      breadcrumbContainer.appendChild(assignedNodes[0].cloneNode(true));
+      this.addSeparator(breadcrumbContainer);
+
+      const ellipsis = document.createElement('span');
+      ellipsis.textContent = '...';
+      breadcrumbContainer.appendChild(ellipsis);
+      this.addSeparator(breadcrumbContainer);
+
+      breadcrumbContainer.appendChild(assignedNodes[assignedNodes.length - 2].cloneNode(true));
+      this.addSeparator(breadcrumbContainer);
+      breadcrumbContainer.appendChild(assignedNodes[assignedNodes.length - 1].cloneNode(true));
+    } else {
+      breadcrumbContainer.innerHTML = '';
+      assignedNodes.forEach((node, index) => {
+        breadcrumbContainer.appendChild(node.cloneNode(true));
+        if (index < assignedNodes.length - 1) {
+          this.addSeparator(breadcrumbContainer);
+        }
+      });
+    }
+  }
+
+  addSeparator(container) {
+    const separator = document.createElement('span');
+    separator.textContent = '|';
+    separator.style.cursor = 'default';
+    container.appendChild(separator);
+  }
+}
+
+class BreadcrumLinkBase extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.render();
+  }
+
+  render() {
+    const href = this.getAttribute('href');
+
+    this.shadowRoot.innerHTML = `
+        <link rel="stylesheet" href="ui/breadcrumb.css" />
+        <a href="${href}" class="breadcrumb__link"><slot></slot></a>
+    `;
+  }
+}
+
+customElements.define('breadcrumb--base', BreadcrumbBase);
+customElements.define('breadcrumb__link--base', BreadcrumLinkBase);
