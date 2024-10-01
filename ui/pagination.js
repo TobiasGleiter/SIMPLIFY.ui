7705883f75be40afc8e60c71b4bc473df7fd04b5
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-10-01 08:04:48 +0200

    chore: setup pagination

---
+++ ui/pagination.js
@@ -0,0 +1,21 @@
+class PaginationBase extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.render();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
+        <link rel="stylesheet" href="ui/pagination.css" />
+
+        <div class="pagination">
+            <button secondary>Previous</button>
+            <div><slot></slot></div>
+            <button>Next</button>
+        </div>
+    `;
+  }
+}
+
+customElements.define('pagination--base', PaginationBase);
