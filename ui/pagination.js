e71a848ea0e03f7d4d0e546612ba77d29b7dba5d
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-10-02 19:35:28 +0200

    chore: pagination changes

--- ui/pagination.js
+++ ui/pagination.js
@@ -8,15 +8,16 @@ class PaginationBase extends HTMLElement {
   render() {
     this.shadowRoot.innerHTML = `
         <link rel="stylesheet" href="ui/pagination.css" />
-
         <div class="pagination">
             <button class="pagination__button" aria-label="Go to previous page">
               <svg class="pagination__previous-arrow" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor"    stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <polyline points="9 18 15 12 9 6"></polyline>
               </svg>
-          Previous</button>
+              <span>Previous</span>
+            </button>
             <div><slot></slot></div>
-            <button class="pagination__button" aria-label="Go to next page">Next
+            <button class="pagination__button" aria-label="Go to next page">
+              <span>Next</span>
               <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor"    stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                   <polyline points="9 18 15 12 9 6"></polyline>
                 </svg>
