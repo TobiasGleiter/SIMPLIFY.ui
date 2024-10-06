901fdffb6ad9d21997605652aff392960a324a52
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-10-06 15:44:14 +0200

    chore: avatar

---
+++ ui/avatar.js
@@ -0,0 +1,21 @@
+class AvatarBase extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.render();
+  }
+
+  render() {
+    const src = this.getAttribute('src');
+
+    this.shadowRoot.innerHTML = `
+        <link rel="stylesheet" href="ui/avatar.css" />
+
+        <div class="avatar">
+            <image src="${src}" alt="user avatar" class="avatar__image"/>
+        </div>
+    `;
+  }
+}
+
+customElements.define('avatar-base', AvatarBase);
